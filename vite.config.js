5ee7fcac63af0367c6a6bef732e9e52a4c6600b4
eunhye <eunhye@eunhyes-MacBook-Pro.local>
2025-01-21 01:19:59 +0900

    feat: add Dockerfile and enable hot-loading

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true, // Use polling for file changes
+    },
+    host: '0.0.0.0', // Allow connections from outside the container
+    port: 5173,      // Match the exposed port
+  },
 })
